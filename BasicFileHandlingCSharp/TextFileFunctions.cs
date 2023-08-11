254492bceed2ed158f7508f75e245b7d20039f42
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2023-08-11 12:41:55 +0000

    Add project files.

---
+++ BasicFileHandlingCSharp/TextFileFunctions.cs
@@ -0,0 +1,27 @@
+﻿namespace BasicFileHandlingCSharp
+{
+	public class TextFileFunctions
+	{
+		private readonly string _rootPath = AppDomain.CurrentDomain.BaseDirectory;
+
+		string _fileName = "FileNameExample.txt";
+
+		public TextFileFunctions( string fileName ) { _fileName = fileName; }
+
+		public void WriteFile( string[] lines )
+		{
+			using StreamWriter writer = new StreamWriter(Path.Combine(_rootPath, _fileName), true);
+			foreach (string line in lines)
+			{
+				writer.WriteLine(line);
+			}
+
+		}
+
+		public string ReadFile()
+		{
+			using StreamReader reader = new StreamReader(Path.Combine(_rootPath, _fileName), true);
+			return reader.ReadToEnd();
+		}
+	}
+}
---
+++ PlayingWithFiles/Program.cs
@@ -0,0 +1,63 @@
+﻿using BasicFileHandlingCSharp;
+
+namespace PlayingWithFiles
+{
+	public class Program
+	{
+		static void Main( string[] args )
+		{
+			int count = 0;
+
+			try
+			{
+				WriteHeadingToScreen();
+
+				Console.WriteLine("Please enter a name for your file");
+
+				string fileName = Console.ReadLine();
+
+				Console.WriteLine();
+
+				string[] lines = new string[3];
+
+				TextFileFunctions textFileFunctions = new(fileName);
+
+				do
+				{
+					Console.WriteLine($"Please add {(count > 0 ? "another line" : "a line")} to file, {fileName}");
+					lines[count] = Console.ReadLine();
+					count++;
+
+				} while (count < 3);
+				textFileFunctions.WriteFile(lines);
+
+				Console.Clear();
+
+				WriteHeadingToScreen();
+
+				Console.WriteLine(textFileFunctions.ReadFile());
+
+
+			}
+			catch (IOException ex)
+			{
+				WriteExceptionToScreen(ex);
+			}
+		}
+
+		private static void WriteHeadingToScreen()
+		{
+			Console.WriteLine("Basic .NET Cross Platform File Handling");
+			Console.WriteLine("_______________________________________");
+			Console.WriteLine();
+		}
+
+		private static void WriteExceptionToScreen( IOException ex )
+		{
+			Console.BackgroundColor = ConsoleColor.DarkRed;
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine(ex.Message);
+			Console.ResetColor();
+		}
+	}
+}
